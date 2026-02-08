58aa54a381ac6e87f0e25526dcc4052c5ebbc3f3
dhruvG110 <guptadhruv472@gmail.com>
2026-02-08 17:43:15 +0530

    Re commit 21

--- app/_not-found.tsx
+++
@@ -1,9 +0,0 @@
-// app/_not-found.tsx
-export default function NotFoundPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-white bg-[#07080f]">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-lg mb-4">Page Not Found</p>
-    </div>
-  );
-}
